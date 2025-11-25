5e171c284816728b6314b41d2514adb1045df160
adilkrgl <adil.krgl@gmail.com>
2025-11-25 22:49:09 +0000

    feat: add authentication domain model

--- ArtiX.Domain/Entities/Core/User.cs
+++ ArtiX.Domain/Entities/Core/User.cs
@@ -1,7 +1,23 @@
 using ArtiX.Domain.Common;
+using ArtiX.Domain.Entities.Identity;
 
 namespace ArtiX.Domain.Entities.Core;
 
 public class User : BaseEntity
 {
+    public string UserName { get; set; } = string.Empty;
+
+    public string Email { get; set; } = string.Empty;
+
+    public byte[] PasswordHash { get; set; } = Array.Empty<byte>();
+
+    public byte[] PasswordSalt { get; set; } = Array.Empty<byte>();
+
+    public bool IsActive { get; set; }
+
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public ICollection<UserRole> UserRoles { get; set; } = new List<UserRole>();
 }
