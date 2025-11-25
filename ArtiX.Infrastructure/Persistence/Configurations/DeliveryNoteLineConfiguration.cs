b91ced703d3c61553520aa748854ba1224aed53c
adilkrgl <adil.krgl@gmail.com>
2025-11-25 20:51:14 +0000

    Configure DeliveryNoteLine product delete behavior

---
+++ ArtiX.Infrastructure/Persistence/Configurations/DeliveryNoteLineConfiguration.cs
@@ -0,0 +1,23 @@
+using ArtiX.Domain.Entities.Sales;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ArtiX.Infrastructure.Persistence.Configurations;
+
+public class DeliveryNoteLineConfiguration : IEntityTypeConfiguration<DeliveryNoteLine>
+{
+    public void Configure(EntityTypeBuilder<DeliveryNoteLine> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasOne(x => x.DeliveryNote)
+            .WithMany()
+            .HasForeignKey(x => x.DeliveryNoteId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Product)
+            .WithMany()
+            .HasForeignKey(x => x.ProductId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
