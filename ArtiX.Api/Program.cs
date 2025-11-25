8dcab93350bc040006a074df6b7e4820359fda67
adilkrgl <adil.krgl@gmail.com>
2025-11-25 11:53:58 +0000

    Register DbContext and add initial migration

--- ArtiX.Api/Program.cs
+++ ArtiX.Api/Program.cs
@@ -1,4 +1,10 @@
+using ArtiX.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ErpDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
